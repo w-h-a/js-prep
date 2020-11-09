a35aeddd10ee323c11d2e98cd4a039c4da475391
Wes Anderson <wesley.herb.anderson@gmail.com>
2020-11-08 20:54:58 -0500

    edited extract.js

--- arrays_exercises/extract.js
+++ arrays_exercises/extract.js
@@ -1,22 +1,25 @@
-// Only works for array with arrays but not arrays with arrays with arrays.
+/*
+** Asks user what type within
+** an arbitrary array they want to search for, and
+** what instance of the type they want to search for.
+** Then it flattens the array and returns the index
+** of the instance (provided it is in the array),
+** otherwise, a "not found" message is returned.
+** The final result is logged.
+*/
 
 function search(inputArrayParam, inputSearchParam) {
-  if (inputSearchParam === undefined) return "I am error."
-  for (let i = 0; i < inputArrayParam.length; i += 1) {
-    if (Array.isArray(inputArrayParam[i])) {
-      if (inputArrayParam[i].includes(inputSearchParam)) {
-        let j = inputArrayParam[i].indexOf(inputSearchParam);
-        return `${inputSearchParam} is at index ${i} and ${j}.`;
-      } else {
-        continue;
-      }
-    } else if (inputArrayParam[i] === inputSearchParam) {
-      return `${inputSearchParam} is at index ${i}.`;
-    } else {
-      continue;
-    }
+
+  let flattenedArray = inputArrayParam.flat(Infinity);
+
+  if (flattenedArray.includes(inputSearchParam)) {
+    let result = flattenedArray.indexOf(inputSearchParam);
+    console.log(`${inputSearchParam} is at index ${result} in the following flattened-version of the array:`);
+    console.log(flattenedArray);
+  } else {
+    console.log(`${inputSearchParam} was not found.`);
   }
-  return `${inputSearchParam} was not found.`
+
 }
 
 function getSearchParam(userParam) {
@@ -39,22 +42,31 @@ function getParam(typeParam) {
       break;
     case 'boolean':
       let booleanInput = getSearchParam("Enter boolean: ");
-      let userBool = (booleanInput === 'true');
-      return Boolean(userBool);
-      break;
+      let userBool;
+      switch (booleanInput) {
+        case 'true':
+          userBool = true;
+          return userBool;
+          break;
+        case 'false':
+          userBool = false;
+          return userBool;
+          break;
+        default:
+          return undefined;
+          break;
+      }
     case 'null':
       return null;
       break;
     default:
-      console.log("Try again!")
+      return undefined;
       break;
   }
 }
 
 let arr = [["test", "hello", "world"], ["example", 6, "mem", null], [4, 8, 12]];
 
-let userInput = getParam("Would you like to search for a string, number, boolean, or null?\n");
 
+let userInput = getParam("Would you like to search for a string, number, boolean, null, or undefined?\n");
 let finalResult = search(arr, userInput);
-
-console.log(finalResult);
